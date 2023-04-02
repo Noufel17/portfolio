3c00f9ed6949b0ff2ca67fd0d06984018a84c195
Noufel17 <jn_naili@esi.dz>
2023-04-02 20:19:50 +0100

    modified fetch url

--- utils/functions.ts
+++ utils/functions.ts
@@ -14,27 +14,25 @@ export const fetchSkills = async () => {
   return skills
 }
 export const fetchExperience = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`,
-  )
+  const res = await fetch(`/api/getExperience`)
   const data = await res.json()
   const experiences: Experience[] = data.experiences
   return experiences
 }
 export const fetchProjects = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
+  const res = await fetch(`/api/getProjects`)
   const data = await res.json()
   const projects: Project[] = data.projects
   return projects
 }
 export const fetchAbout = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getAbout`)
+  const res = await fetch(`/api/getAbout`)
   const data = await res.json()
   const about: About = data.about
   return about
 }
 export const fetchContact = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getContact`)
+  const res = await fetch(`/api/getContact`)
   const data = await res.json()
   const contactInfo: ContactInfo = data.contactInfo
   return contactInfo
